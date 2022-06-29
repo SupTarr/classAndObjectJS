9b271057673469021a95e0663fb1fecef61dfdc0
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-06-29 20:49:01 +0700

    Class & Object

---
+++ Build a Library.js
@@ -0,0 +1,98 @@
+class Media {
+    constructor(title) {
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+    get title() {
+      return this._title;
+    }
+    get isCheckedOut() {
+      return this._isCheckedOut;
+    }
+    get ratings() {
+      return this._ratings;
+    }
+    toggleCheckOutStatus() {
+      this._isCheckedOut = !this._isCheckedOut;
+    }
+    getAverageRating() {
+      const sum = this._ratings.reduce((acc, cur) => {return acc += cur;});
+      console.log(sum);
+      const n = this._ratings.length;
+      console.log(n);
+      return sum/n;
+    }
+    addRating(rating) {
+      if (rating >= 1 && rating <= 5) {
+        return this._ratings.push(rating);
+      }
+    }
+    set isCheckedOut(isCheckedOut) {
+      this._isCheckedOut = isCheckedOut;
+    }
+  }
+  
+  class Book extends Media {
+    constructor(author, title, pages) {
+      super(title);
+      this._author = author;
+      this._pages = pages;
+    }
+    get author() {
+      return this._author;
+    }
+    get pages() {
+      return this._pages;
+    }
+  }
+  
+  class Movie extends Media {
+    constructor(director, title, runTime) {
+      super(title);
+      this._director = director;
+      this._runTime = runTime;
+    }
+    get director() {
+      return this._director;
+    }
+    get runTime() {
+      return this._runTime;
+    }
+  }
+  
+  class CD extends Media {
+    constructor(artist, title, songs) {
+      super(title);
+      this._artist = artist;
+      this._songs = songs;
+    }
+    get artist() {
+      return this._artist;
+    }
+    get songs() {
+      return this._songs;
+    }
+    static shuffle() {
+      const randomNumber = Math.floor(Math.random()*this._songs.length);
+      return this._songs[randomNumber];
+    }
+  }
+  
+  const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+  historyOfEverything.toggleCheckOutStatus();
+  console.log(historyOfEverything.isCheckedOut);
+  historyOfEverything.addRating(4);
+  historyOfEverything.addRating(5);
+  historyOfEverything.addRating(5);
+  console.log(historyOfEverything.ratings);
+  console.log(historyOfEverything.getAverageRating());
+  
+  const speed = new Movie('Jan de Bont', 'Speed', 116);
+  speed.toggleCheckOutStatus();
+  console.log(speed.isCheckedOut);
+  speed.addRating(1);
+  speed.addRating(1);
+  speed.addRating(5);
+  console.log(speed.ratings);
+  console.log(speed.getAverageRating());
